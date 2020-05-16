27a93fa790e7aad31aa714f464d9b7547f67ea93
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-05-16 01:20:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 let body =document.querySelector('body')
 let main =document.querySelector('#main')
 body.removeChild(main)
-for(i=0;i<3;i++){
-  let newHeader = document.createElement('header')
-  newHeader.setAttribute('id','victory')
-  newHeader.innerHTML = "Ebraheem"
-  if(i==2){
-    newHeader.innerHTML = 'Ebraheem is the champion'
-    
-  }body.appendChild(newHeader)
-}
+let newHeader = document.createElement('header')
+newHeader.setAttribute('id','victory')
+newHeader.innerHTML = "Ebraheem"
+
+
+body.appendChild(newHeader)
 
 
