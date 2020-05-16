bcd88f8f523e026d23db01daf6dcaa75b71969e5
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-05-16 01:23:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+let body =document.querySelector('body')
+let main =document.querySelector('#main')
+body.removeChild(main)
+let myName='Ebraheem'
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.innerHTML = ''+myName+' is the champion'
+
+
+body.appendChild(newHeader)
+
+
