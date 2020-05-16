b0622cf14e3d1fbc1f39347e9b4f5ebb22515590
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-05-16 01:11:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let main =document.querySelector('#main')
 body.removeChild(main)
 for(i=0;i<3;i++){
   let newHeader = document.createElement('h1')
+  newHeader.
   newHeader.innerHTML = "Ebraheem"
   if(i==3){
     newHeader.innerHTML = "Ebraheem is the hero"
